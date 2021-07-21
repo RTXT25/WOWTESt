3014e248b8fbb5d6850793c1c5f9004d2dbd64ff
adsaf123 <filipkopyt14@gmail.com>
2021-07-21 19:04:56 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -71,7 +71,7 @@ addLayer("BA", {
             player[this.layer].currentFrame++
             video.currentTime = player[this.layer].currentFrame / 20
             ctx.drawImage(video, 0, 0, 32, 24)
-            var data = ctx.getImageData(x, y, 32, 24).data
+            var data = ctx.getImageData(0, 0, 32, 24).data
             for (var x = 1; x <= 32; x++) {
                 for (var y = 1; y <= 24; y++) {
                     setGridData(this.layer, formatGridId(x, y), "#" + data[4 * ((x-1)*24 + y)].toString(16) + data[4 * ((x-1)*24 + y) + 1].toString(16) + data[4 * ((x-1)*24 + y) + 2].toString(16))
